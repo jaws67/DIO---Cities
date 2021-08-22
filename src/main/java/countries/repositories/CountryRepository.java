97a5516d4fc245e2b7474610c562d2a3c35fa00f
jaws67 <86533771+jaws67@users.noreply.github.com>
2021-08-21 21:02:28 -0300

    Add files via upload

---
+++ src/main/java/countries/repositories/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.github.andrelugomes.countries.repositories;
+//rever
+
+import com.github.andrelugomes.countries.entities.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/main/java/staties/repositories/StateRepository.java
@@ -0,0 +1,8 @@
+package com.github.andrelugomes.staties.repositories;
+//rever
+
+import com.github.andrelugomes.staties.entities.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
